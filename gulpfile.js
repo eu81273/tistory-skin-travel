650a27b1330f8b70e456dfd61fd685477b6b93a9
Jaeha Ahn <eu81273@gmail.com>
2016-06-07 23:34:52 +0900

    Initial commit :rocket:

---
+++ gulpfile.js
@@ -0,0 +1,76 @@
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var cleanCss = require('gulp-clean-css');
+var browserSync = require('browser-sync').create();
+var plumber = require('gulp-plumber');
+
+//자바스크립트 파일을 하나로 합치고 압축한다.
+gulp.task('optimize-js', function () {
+	return gulp.src(['src/js/!(main).js', 'src/js/main.js'])
+		.pipe(plumber())
+		.pipe(concat('main.min.js'))
+		.pipe(uglify())
+		.pipe(gulp.dest('dist/images'))
+		.pipe(browserSync.reload({stream: true}));
+});
+
+//CSS 파일을 하나로 합치고 압축한다.
+gulp.task('optimize-css', function () {
+	return gulp.src(['src/css/!(style).css', 'src/css/style.css'])
+		.pipe(plumber())
+		.pipe(concat('style.css'))
+		.pipe(cleanCss({compatibility: 'ie10'}))
+		.pipe(gulp.dest('dist/'))
+		.pipe(browserSync.reload({stream: true}));
+});
+
+//HTML 파일을 압축한다.
+gulp.task('concat-html', function () {
+	return gulp.src(['src/commonHeader.html', 'src/content*.html', 'src/commonFooter.html'])
+		.pipe(plumber())
+		.pipe(concat('skin.html'))
+		.pipe(gulp.dest('dist/'))
+		.pipe(browserSync.reload({stream: true}));
+});
+
+//이미지 파일을 복사한다.
+gulp.task('serve-images', function () {
+	return gulp.src('src/images/**/*')
+		.pipe(plumber())
+		.pipe(gulp.dest('dist/images'));
+});
+
+
+//index.xml 파일을 복사한다.
+gulp.task('serve-xml', function () {
+	return gulp.src('src/index.xml')
+		.pipe(plumber())
+		.pipe(gulp.dest('dist/'));
+});
+
+//Static Server 를 시작한다.
+gulp.task('static-server', ['build'], function () {
+	// dist 디렉토리를 기준으로 웹서버 시작
+	browserSync.init({
+			server: {
+					baseDir: "./dist",
+					index: "skin.html"
+			}
+	});
+});
+
+//전체 빌드를 수행한다.
+gulp.task('build', ['serve-xml', 'serve-images', 'optimize-js', 'optimize-css','concat-html']);
+
+// 파일 변경 감지
+gulp.task('watch', function () {
+	gulp.watch('src/js/*.js', ['optimize-js']);
+	gulp.watch('src/css/*.css', ['optimize-css']);
+	gulp.watch('src/*.html', ['concat-html']);
+	gulp.watch('src/index.xml', ['serve-xml']);
+	gulp.watch('src/images/*', ['serve-images']);
+});
+
+//기본 task 설정
+gulp.task('default', ['static-server', 'watch' ]);
---
+++ src/js/main.js
@@ -0,0 +1,126 @@
+$(document).ready(function () {
+
+	var $navigation = $('nav, .blog-overlay');
+	var $article = $('article');
+	var $header = $('.blog-logo, .blog-open-nav');
+	var $posting = $('.blog-post');
+	var $images = $('[class^="background-picture"]');
+
+	var previousPostion = 0;
+	var previousDirection = undefined;
+
+	var currentPostion = 0;
+	var currentDirection = undefined;
+
+	//스크롤링을 하면,
+	$('.blog-post').on('scroll resize', function (event) {
+		//현재 위치 구하기
+		currentPostion = $posting.scrollTop();
+
+		//변화가 있는 경우에만,
+		if (previousPostion !== currentPostion) {
+			//현재 방향 구하기
+			currentDirection = previousPostion < currentPostion;
+			//포지션 업데이트
+			previousPostion = currentPostion;
+
+			//방향이 변화된 경우,
+			if (previousDirection !== currentDirection) {
+				//방향 업데이트
+				previousDirection = currentDirection;
+				//아래쪽 스크롤인 경우 숨기기
+				$header.toggleClass('is-hide', currentDirection);
+			}
+		}
+
+		return false;
+	});
+
+	//메뉴 버튼을 누르면,
+	$('.blog-open-nav').on('click', function (event) {
+		//History API를 지원하는 경우에만,
+		if (typeof history.pushState === 'function') {
+			history.pushState('open-nav', 'nav');
+		}
+
+		$navigation.toggleClass('is-visible', true);
+		$article.toggleClass('scale-down', true);
+		$images.toggleClass('is-visible', true);
+
+		return false;
+	});
+
+	//백버튼을 위해 히스토리 API 활용
+	$(window).on('popstate', function (event) {
+		//jQuery의 경우 event가 아닌 originalEvent 에 state가 저장됨
+		var status = event.state || event.originalEvent.state;
+
+		//메뉴가 오픈된 경우
+		if (status == 'open-nav') {
+			$navigation.toggleClass('is-visible', true);
+			$article.toggleClass('scale-down', true);
+			$images.toggleClass('is-visible', true);
+		}
+
+		else {
+			$navigation.toggleClass('is-visible', false);
+			$article.toggleClass('scale-down', false);
+			$images.toggleClass('is-visible', false);
+		}
+
+		return false;
+	});
+
+	//닫기 버튼이나 오버레이를 누르면,
+	$('.blog-close-nav, .blog-overlay').on('click', function (event) {
+		//History API를 지원하는 경우에만,
+		if (typeof history.pushState === 'function') {
+			history.back();
+		}
+
+		$navigation.toggleClass('is-visible', false);
+		$article.toggleClass('scale-down', false);
+
+		return false;
+	});
+
+	//검색을 시도하면,
+	$('#search').on('keypress', function (event) {
+		//정상적으로 검색어를 입력한 경우,
+		if(event.keyCode === 13 && $(this).val().length > 0) {
+			window.location.href = "/search/" + $(this).val().replace(new RegExp("%", "g"), "%25").replace(new RegExp("\\?", "g"), "%3F").replace(new RegExp("#", "g"), "%23");
+			return false;
+		}
+	});
+
+	//포스팅 삭제를 시도하면,
+	$('#delete').on('click', function (event) {
+		var url = 'http://' + window.location.hostname;
+		var post = $(this).attr("href");
+
+		if (window.confirm('이 포스트를 삭제하시겠습니까?')) {
+			$.ajax({
+				url: url + '/oldmin/entry/removeEntry' + post + '?__T__=' + Number(new Date()),
+				method: 'get',
+				headers: {
+					'X-Alt-Referer': url
+				},
+				success: function () {
+					window.location.href = url;
+				}
+			});
+		}
+
+		return false;
+	});
+
+	//이미지를 감싼 imageblock 클래스 제거
+	$('.imageblock > img').unwrap();
+
+	//prism 호환성 추가
+	$("pre[class^='brush']").each(function (index, element, language) {
+		language = element.className && element.className.substr(6);
+		language = ("text" == language || "html" == language || !language) && "markup" || "js" == language && "javascript" || language;
+		$(element).removeClass().wrapInner('<code class="language-' + language + '"></code>');
+	});
+});
